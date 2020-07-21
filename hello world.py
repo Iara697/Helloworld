af9b507edb8148005483dedb582783944e650473
Iara Macedo <iara.mdgomes@gmail.com>
2020-07-21 11:45:59 -0300

    Hello world python

---
+++ hello world.py
@@ -0,0 +1,10 @@
+print("Digite o nome da empresa")
+truly = str (input())
+
+print("Digite seu nome")
+nomeFuncionario = str (input())
+
+print ("Olá " + nomeFuncionario + " da " + Truly + " eu sou a Iara")
+
+
+
